a1d57c9d565646540b3ede95f189781ca55ca308
Adnan Khan <adnan.khan.nustian@gmail.com>
2023-05-10 14:37:52 +0500

    added haltApk function for stopping stage apk rollout.

--- apk_upload/src/main/kotlin/me/szymanski/apkupload/Main.kt
+++ apk_upload/src/main/kotlin/me/szymanski/apkupload/Main.kt
@@ -8,23 +8,22 @@ import com.google.api.services.androidpublisher.AndroidPublisher
 import com.google.api.services.androidpublisher.AndroidPublisherScopes
 import com.google.api.services.androidpublisher.model.AppEdit
 import com.google.api.services.androidpublisher.model.Bundle
+import com.google.api.services.androidpublisher.model.Track
 import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.ServiceAccountCredentials
 import java.io.File
 import java.io.FileInputStream
-import java.lang.Exception
 import kotlin.system.exitProcess
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         try {
-            println("Started apk upload script")
             val appId = args[0]
-            val apkPath = args[1]
+            val trackName = args[1]
             val credentialsPath = args[2]
-            println("Received arguments\nappId: $appId\napkPath: $apkPath\ncredentialsPath: $credentialsPath")
-            uploadApk(appId, apkPath, credentialsPath)
+            println("Received arguments\nappId: $appId\ntrackName: $trackName\ncredentialsPath: $credentialsPath")
+            haltApk(appId, trackName, credentialsPath)
         } catch (e: Exception) {
             println("${e::class.simpleName} ${e.message}")
             exitProcess(SCRIPT_FAILED_CODE)
@@ -65,5 +64,41 @@ object Main {
 
         publisher.edits().commit(appId, edit.id).execute()
         println("Committed edit with a new apk")
+    }
+
+
+    private fun haltApk(appId: String, trackName: String, credentialsPath: String) {
+        val credentials = ServiceAccountCredentials
+            .fromStream(FileInputStream(credentialsPath))
+            .createScoped(AndroidPublisherScopes.all())
+
+        val publisher = AndroidPublisher.Builder(
+            GoogleNetHttpTransport.newTrustedTransport(),
+            JacksonFactory.getDefaultInstance(),
+            setHttpTimeout(HttpCredentialsAdapter(credentials))
+        ).setApplicationName("Google Play APK halting").build()
+
+        val edit: AppEdit = publisher.edits().insert(appId, null).execute()
+
+        println("Created edit: ${edit.id}")
+
+        val track: Track = publisher.edits().tracks()
+            .get(appId, edit.id, trackName)
+            .execute();
+
+        // Set the `halted` parameter to `true` for all releases in the track
+        val release = track.releases[0]
+        release.userFraction = 0.0;
+        release.status = "halted"
+        println(release.versionCodes)
+
+        // Update the track on the Play Console with the halted releases
+        val updatedTrack: Track = publisher.edits().tracks()
+            .update(appId, edit.id, trackName, track)
+            .execute();
+
+        println("Staged rollout for track '" + updatedTrack.track + "' has been halted.");
+
+        publisher.edits().commit(appId, edit.id).execute()
     }
 }
